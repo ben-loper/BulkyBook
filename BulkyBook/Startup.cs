583764a2cd778d1a6b1c4e0fc1b7797988de71df
Ben Loper <loperbp@gmail.com>
2020-06-07 13:42:56 -0400

    Add runtime compliation for Razor pages, update welcome message

--- BulkyBook/Startup.cs
+++ BulkyBook/Startup.cs
@@ -30,10 +30,10 @@ namespace BulkyBook
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
